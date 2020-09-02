5dc7b666a4ccd347d3d747835dfd9baaebd3d284
Ethan Jones <ethanjones800@yahoo.com>
2020-09-02 17:09:26 -0500

    Added a battle system. Added cases. Added second decision in explore.

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace HelloWorld
@@ -7,61 +8,144 @@ namespace HelloWorld
     class Game
     {
         bool _gameOver = false;
-        string _playerName = "none";
-
-        void NewName()
+        string _playerName = "player";
+        int playerHealth = 100;
+        void RequestName(ref string name)
         {
-            if (_playerName != "")
-            {
-                return;
-            }
 
 
 
             char input = ' ';
+            Console.ReadKey();
             while (input != '1')
             {
+
+
                 Console.Clear();
                 Console.WriteLine("Please enter your name loser.");
-                _playerName = Console.ReadLine();
+                name = Console.ReadLine();
                 Console.WriteLine("Hello " + _playerName);
-                Console.WriteLine("Are you sure you want the name " + _playerName + "?");
-                input = GetInput("Yes", "No", "Are you sure want that dumb name " + _playerName + "?");
+                input = GetInput("Yes", "No", " Are you sure want that dumb name " + _playerName + "?");
+                if (input == '1')
+                {
+                    return;
+                }
+
+
+
                 if (input == '2')
                 {
-                    Console.WriteLine("Stop wasting my time and pick a name ");
+                    Console.WriteLine(" Stop wasting my time and pick a name ");
+                    RequestName(ref name);
                 }
+                Console.WriteLine(" Name accepted");
             }
-
         }
 
         void Explore()
         {
+            string petName = "Huge boulder";
             char input = ' ';
-            Console.WriteLine(".");
             input = GetInput("Press 1 to go white", "Press 2 to go leaf", " You find a pat");
             if (input == '1')
             {
-                Console.WriteLine("You went write and meet a weirded cat that borks.");
-                Console.WriteLine("It wants to fite you.");
-
+                Console.WriteLine(" Are you stupid! GO RITE BECAUSE ITS RIGHT!");
+                Console.WriteLine(" Whutever, just gone." + "You see a dragon.");
+                Console.WriteLine(" Dragon chases you, then random" + petName + "lands on the dragon (with no context where the boulder came from).");
+                Console.WriteLine(" Boulder seems to be following you, you might as well keep it. Give it a name NOW!");
+                Console.WriteLine(" New name is " + petName);
+                Console.ReadLine();
+                Console.ReadKey();
+                Console.WriteLine("");
 
             }
-
             else if (input == '2')
             {
-                Console.WriteLine("Are you stupid! GO RITE BECAUSE ITS RIGHT.");
-                Console.WriteLine("Whutever, just gone.");
-                Console.WriteLine("You see a dragon");
-                Console.WriteLine("It kills you, I laughed cuz you dead.");
-                _gameOver = true;
-            }
+                Console.WriteLine(" You went write and meet a weirded cat that borks.");
+                Console.WriteLine(" It wants to fite you.");
+                Console.ReadKey();
 
+            }
+            Console.Clear();
+            Console.WriteLine(" Put something here later.");
+
+            int enemyHealth = 75;
+            _gameOver = StartBattle(ref playerHealth, ref enemyHealth);
+            Console.Clear();
 
         }
+        void EnterRoom(int roomNumber)
+        {
+            string exitMessage = "";
+            switch (roomNumber)
+            {
+                case 0:
+                    exitMessage = " You depart away from the L shape castle.";
+                    Console.WriteLine(" You see a L shape castle laughing at you ,cuz you stepped on bird poop.");
+                    break;
+
+                case 1:
+                case 2:
+                    {
+                        exitMessage = " You left the L shape castle";
+                        Console.WriteLine(" You enter the castle. There are rooms and a hallway to go to and, the paintings are talking about how ugly you are.");
+                        break;
+
+                    }
+
+                default:
+                    {
+                        exitMessage = " You left the hallway";
+                        Console.WriteLine(" You decide to stay in the hallway");
+                        break;
+
+                    }
+
+
+
+            }
+            Console.WriteLine(roomNumber);
+            char input = ' ';
+            input = GetInput(" Go forward", "Go back", "Which direction would you like to go?");
+            if (input == '1')
+            {
+                EnterRoom(roomNumber + 1);
+            }
+            Console.WriteLine(exitMessage);
+        }
+
+        bool StartBattle(ref int playerHealth, ref int enemyHealth)
+        {
+            char input = ' ';
+            while (playerHealth > 0 && enemyHealth > 0)
+            {
+                input = GetInput("Attack", "Defend", "What will you do?");
+                if (input == '1')
+                {
+                    enemyHealth -= 10;
+                    Console.WriteLine("You attacked and did something damage");
+                }
+                else if (input == '2')
+                {
+                    Console.WriteLine("You blocked the enemy's attack!");
+                    Console.ReadKey();
+                    continue;
+                }
+                playerHealth -= 20;
+                Console.WriteLine("The enemy attacked and did something damage!");
+                Console.ReadKey();
+            }
+            return playerHealth <= 0;
+            
+        }
+
+
         void ViewStats()
         {
-            // Console.WriteLine("_playerName");
+            Console.WriteLine(_playerName);
+            Console.Write("> ");
+            Console.ReadKey();
+
 
 
         }
@@ -103,7 +187,6 @@ namespace HelloWorld
             while (_gameOver == false)
             {
                 Update();
-                Explore();
             }
 
             End();
@@ -122,7 +205,9 @@ namespace HelloWorld
         //Used for all game logic that will repeat
         public void Update()
         {
-            NewName();
+            RequestName(ref _playerName);
+            Explore();
+            EnterRoom(0);
         }
 
         //Performed once when the game ends
